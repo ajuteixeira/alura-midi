1f8a647ece366a435ee0e9c394a54bba8d3277e6
ajuteixeira <teixeirajuuh@gmail.com>
2022-07-31 04:03:44 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`; // template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    };
+}
