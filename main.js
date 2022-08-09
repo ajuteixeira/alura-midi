7154a9b013542f6d106d2a3206de6dc7ae79f909
ajuteixeira <teixeirajuuh@gmail.com>
2022-08-09 12:16:58 -0300

    first commit

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+  
+  if (elemento === null) {
+    alert('Elemento não encotrado ou seletor inválido');
+  }
+
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  }
+
+  else {
+    alert('Elemento não encotrado ou seletor inválido');
+  }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
 
-    const tecla = listaDeTeclas[i];
-    const instrumento = tecla.classList[1];
+  const tecla = listaDeTeclas[i];
+  const instrumento = tecla.classList[1];
 
-    const idAudio = `#som_${instrumento}`; // template string
+  const idAudio = `#som_${instrumento}`; // template string
 
-    tecla.onclick = function () {
-        tocaSom(idAudio)
-    };
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+
 }
-
